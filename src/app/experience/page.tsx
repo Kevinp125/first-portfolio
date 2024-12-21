375617db3e7ed3f193e41fc1b02ecbc95ba4fb84
Kevin Pereda <kptwdo15@gmail.com>
2024-12-21 17:21:22 -0500

    got a deployment error cause I needed to write down an apostrophe inside of a <p> tag with a certain code

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,6 +1,25 @@
 import Header from "../(components)/Header";
 
 export default function app() {
+  
+  const experience = [
+    {
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+  ]
   return (
     <div className= "min-h-screen flex flex-col items-center ml-44">
 
@@ -9,13 +28,13 @@ export default function app() {
         <Header header = "Experience" icon = "Experience"/>
 
         <div className = "mt-5">
-          <h2 className = "text-lg max-w-2xl wrap">My journey in tech has been shaped by amazing opportunities and experiences. Here’s a quick glimpse at the path I’ve taken so far.</h2>
+          <h2 className = "text-lg max-w-2xl wrap">My journey in tech has been shaped by amazing opportunities and experiences. Here&apos;s a quick glimpse at the path I&apos;ve taken so far.</h2>
         </div>
 
         <div className = "flex ">
           
           <div className="w-28 min-h-screen bg-contain scroll -ml-12 mt-6" style ={{backgroundImage: "url('/images/bamboo.png')"}}></div>
-          
+
         </div>
         
       </div>
