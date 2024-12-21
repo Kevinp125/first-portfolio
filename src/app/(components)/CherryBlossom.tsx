65049932e456628d82b171f9c55381ed840758b3
Kevin Pereda <kptwdo15@gmail.com>
2024-12-21 17:19:12 -0500

    Changed header to be absolute so if user scrolls it leaves the webpage also working on exeprience route now. I have the header and the bar that will work as like my timeline of sorts where all my experience cards are going to go

--- src/app/(components)/CherryBlossom.tsx
+++ src/app/(components)/CherryBlossom.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function CherryBlossom() {
   return(
-    <Image className = "absolute -left-12 -top-8 rotate-45 z-50 pointer-events-none" src = "/images/cherryBlossom.webp" alt = "Kevin Cherry Blossom" width = {500} height = {600}/>
+    <Image className = "fixed -left-12 -top-8 rotate-45 z-50 pointer-events-none " src = "/images/cherryBlossom.webp" alt = "Kevin Cherry Blossom" width = {450} height = {550}/>
 
   );
 
--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -2,6 +2,24 @@ import Header from "../(components)/Header";
 
 export default function app() {
   return (
-    <Header header = "Experience" icon = "Experience"/>
+    <div className= "min-h-screen flex flex-col items-center ml-44">
+
+      <div className= "flex flex-col items-start mt-44">
+
+        <Header header = "Experience" icon = "Experience"/>
+
+        <div className = "mt-5">
+          <h2 className = "text-lg max-w-2xl wrap">My journey in tech has been shaped by amazing opportunities and experiences. Here’s a quick glimpse at the path I’ve taken so far.</h2>
+        </div>
+
+        <div className = "flex ">
+          
+          <div className="w-28 min-h-screen bg-contain scroll -ml-12 mt-6" style ={{backgroundImage: "url('/images/bamboo.png')"}}></div>
+          
+        </div>
+        
+      </div>
+
+    </div>
   );
 }
