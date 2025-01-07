80fdf02d7fd6b37345cee835e1f0b78f17027c65
Kevin Pereda <kptwdo15@gmail.com>
2025-01-06 23:03:06 -0500

    added animations for skill page as well. Finally done with like the desktop port of my website now just need to work on making it funciton on other screensizes as well. Still not fully convinced with animation for skill labels (languages, web, etc) but can always edit later it is currently fade down i I think

--- src/app/(components)/IconCard.tsx
+++ src/app/(components)/IconCard.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 
-export default function IconCard({src, alt}: {src:string, alt:string}) {
+export default function IconCard({src, alt, className, style}: {src:string, alt:string, style: React.CSSProperties, className: string}) {
   return (
-    <div className = "flex flex-row justify-center items-center bg-navbarFill border-3 border-pink-300 rounded-xl w-36 h-36 object-contain">
+    <div style = {style} className = {`${className} opacity-0 flex flex-row justify-center items-center bg-navbarFill border-3 border-pink-300 rounded-xl w-36 h-36 object-contain`}>
       <Image className = "select-none p-3" src = {src} alt = {alt} width = {100} height = {100}/>
     </div>
     
--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -42,7 +42,7 @@ export default function app() {
 
       <div className = "flex flex-cols items-start pl-44 pt-48"> {/*This div is to align the <p> tag with our header */}
         <Header header = "Skills" icon = "Skills"/>
-        <p className = "text-lg max-w-2xl wrap ">Throughout my journey of studying computer science and working on various projects, I&apos;ve had the opportunity to work with and experiment with a diverse range of technologies, tools, and frameworks. Here&apos;s a glimpse of what I&apos;ve explored along the way. </p>
+        <p className = "animate-fade animate-once animate-duration-[2000ms] animate-normal animate-fill-forwards text-lg max-w-2xl wrap ">Throughout my journey of studying computer science and working on various projects, I&apos;ve had the opportunity to work with and experiment with a diverse range of technologies, tools, and frameworks. Here&apos;s a glimpse of what I&apos;ve explored along the way. </p>
       </div>
       
       <div className = "grid grid-cols-1 gap-16 pt-12 pl-40"> {/*Here we define a grid of only one column so that all of our sections "sibling divs" can stack on each other */}
@@ -50,11 +50,12 @@ export default function app() {
         {/*languages section we wrap the header and nested grid in a div so that tailwind knows both of those take up one row */}
         <div>
 
-          <h1 className = "flex flex-col items-center text-4xl pb-5">Languages</h1>
+          <h1 className = "animate-fade-down animate-once animate-duration-[1500ms] animate-ease-in animate-normal animate-fill-forwards flex flex-col items-center text-4xl pb-5">Languages</h1>
           {/*This is a nested grid of 4 cols didnt declared the rows because they will be auto created as cols fill up but this grid is for icons which will be rendered through the IconCard component. We map through languageIcons array of objects to get each object */}
           <div className = "grid grid-cols-4 gap-4">
-            {languageIcons.map(({src, alt}) => (
-              <IconCard key = {src} src = {src} alt = {alt}/>
+            {/*For below IconCard compoment and other ones below we have style and className passed as props so we can animate them within the component itself */}
+            {languageIcons.map(({src, alt}, i) => (
+              <IconCard style = {{animationDelay: `${i*0.2}s`}} className = "animate-fade animate-once animate-duration-[2500ms] animate-ease-out animate-normal animate-fill-forwards" key = {i} src = {src} alt = {alt}/>
             ))}
           </div>
 
@@ -64,12 +65,12 @@ export default function app() {
 
         <div>
 
-          <h2 className = "flex flex-col items-center text-4xl pb-5"> Web</h2>
+          <h2 className = "animate-fade-down animate-once animate-duration-[1500ms] animate-ease-in animate-normal animate-fill-forwards flex flex-col items-center text-4xl pb-5"> Web</h2>
 
           <div className = "grid grid-cols-4 gap-4">
             {/*We map through webIcons array of objects to get each object and pass in the values to the IconCard component */}
-            {webIcons.map(({src, alt}) => (
-              <IconCard key = {src} src = {src} alt = {alt}/>
+            {webIcons.map(({src, alt}, i) => (
+              <IconCard style = {{animationDelay: `${i*0.2}s`}} className = "animate-fade animate-once animate-duration-[2500ms] animate-ease-out animate-normal animate-fill-forwards" key = {i} src = {src} alt = {alt}/>
             ))}
           </div>
 
@@ -81,12 +82,12 @@ export default function app() {
 
         <div>
 
-          <h3 className = "flex flex-col items-center text-4xl pb-6"> Tools </h3>
+          <h3 className = "animate-fade-down animate-once animate-duration-[1500ms] animate-ease-in animate-normal animate-fill-forwards flex flex-col items-center text-4xl pb-6"> Tools </h3>
 
           <div className = "grid grid-cols-4 gap-4 pb-5">
             {/*We map through webIcons array of objects to get each object and pass in the values to the IconCard component*/}
-            {toolIcons.map(({src, alt}) => (
-              <IconCard key = {src} src = {src} alt = {alt}/>
+            {toolIcons.map(({src, alt}, i) => (
+              <IconCard style = {{animationDelay: `${i*0.2}s`}} className = "animate-fade animate-once animate-duration-[2500ms] animate-ease-out animate-normal animate-fill-forwards" key = {i} src = {src} alt = {alt}/>
             ))}
           </div>
 
