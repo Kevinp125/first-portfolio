e81647e29aaae5c8928542a73358b70807fbaa6f
Kevin Pereda <kptwdo15@gmail.com>
2025-05-06 15:34:10 -0400

    Made fixed cherry blossom on top left corner of the screen change sizes and location depending on the screen size.

--- src/app/(components)/CherryBlossom.tsx
+++ src/app/(components)/CherryBlossom.tsx
@@ -2,7 +2,19 @@ import Image from "next/image";
 
 export default function CherryBlossom() {
   return(
-    <Image className = "select-none fixed -left-12 -top-8 rotate-45 z-50 pointer-events-none " src = "/images/cherryBlossom.webp" alt = "Kevin Cherry Blossom" width = {450} height = {550}/>
+    <Image
+      className = 
+      {`select-none fixed top-0 left-0 origin-top-left rotate-45 z-50 pointer-events-none 
+
+        /* translate: smaller on phones, larger on tablets, full on desktop */
+        translate-x-24  -translate-y-20      /* default (<640 px) */
+        sm:translate-x-24 sm:-translate-y-16  /* 640‑767 px */
+        md:translate-x-36 md:-translate-y-32  /* 768 px and up */
+
+        scale-75  sm:scale-90 md:scale-100`} 
+      src = "/images/cherryBlossom.webp" 
+      alt = "Kevin Cherry Blossom" 
+      width = {450} height = {550}/>
 
   );
 
