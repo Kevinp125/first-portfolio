a1c0cf75170f4c0299c34482cadf9db46e846973
Kevin Pereda <kptwdo15@gmail.com>
2025-05-06 15:06:47 -0400

    Added smaller screeen responsiveness to the navbar. Now on smaller screen (tablets and phones) it becomes a flex row and gets moved to the bottom of the screen similar to an iios dock.

--- src/app/(components)/Navbar.tsx
+++ src/app/(components)/Navbar.tsx
@@ -20,7 +20,19 @@ export default function Navbar (){
   
   return(
     <nav
-    className = "fixed right-12 top-1/2 transform -translate-y-1/2 w-16 h-96 bg-navbarFill rounded-3xl flex flex-col items-center justify-evenly shadow-xl border-4 border-navbarBorder z-10">{/*fixed makes the navbar stay in the same place on the screen, right-10 moves it 10 units from the right side of the screen, top-1/2 moves it 50% from the top of the screen, transform -translate-y-1/2 moves it up 50% of its height*/}
+    className = {`
+    /* ─────────────────────────── MOBILE + MEDIUM (default) ─────────────────────────── */
+    fixed bottom-0 inset-x-0
+    h-16 flex items-center justify-evenly
+    bg-navbarFill/80 backdrop-blur-md safe-bottom
+    border-4 border-navbarBorder
+    
+
+    /*───────────────────────────── DESKTOP ≥ lg (override) ───────────────────────────*/
+
+    lg:left-auto lg:right-12 lg:top-1/2 transform lg:-translate-y-1/2 
+    lg:w-16 lg:h-96 bg-navbarFill rounded-3xl 
+    lg:flex-col shadow-xl z-10`}>{/*fixed makes the navbar stay in the same place on the screen, right-10 moves it 10 units from the right side of the screen, top-1/2 moves it 50% from the top of the screen, transform -translate-y-1/2 moves it up 50% of its height*/}
 
     {/*mapping through the links array and creating a Link tag for each link in the array, just easier and better code practice so we dont have to
       manually change a bunch of Link tags now if we want to update any specific link just go to link array*/}
@@ -31,7 +43,7 @@ export default function Navbar (){
 
     {links && links.map( ({href, icon, hoverLabel}) => (
       <Link key = {href} href = {href} className = "group relative group"> {icon} 
-        <span className = "absolute right-10 -top-0.5 text-sm bg-zinc-700 text-white p-0.5 pl-1 rounded-sm opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out pointer-events-none"> {hoverLabel} 
+        <span className = "hidden md:block absolute right-10 -top-0.5 text-sm bg-zinc-700 text-white p-0.5 pl-1 rounded-sm opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out pointer-events-none"> {hoverLabel} 
 
           <span className = "absolute top-1/2 transform -translate-y-1/2 -translate-x-2 w-4 h-4 bg-zinc-700 rotate-45 -z-10"></span>
 
