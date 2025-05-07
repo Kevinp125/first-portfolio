c86eb5bd1d00172180b79405e008443f7f69127e
Kevin Pereda <kptwdo15@gmail.com>
2025-05-07 14:54:25 -0400

    Changed it by calling the ScrollLock component instead of making the whole page use client by having a useeffect inside of jsx being returned. Also adjusted mountain to be further up so that safari bar doesnt cover it.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,23 @@
-"use client";
-
 import Image from "next/image";
-import { useEffect } from "react";
 import Header from "./(components)/Header";
+import ScrollLock from "./(components)/ScrollLock";
 
 const introductionText: string = "Hi, I'm Kevin Pereda — software engineer and undergraduate student at UCF."; //declaring a string variable that will hold the introduction text that will be displayed on the home page
 
 export default function Home() {
-
-  useEffect(() => {
-    const html = document.documentElement;
-    const body = document.body;
-
-    // stash originals (in case other pages changed them)
-    const htmlOverflow = html.style.overflow;
-    const bodyOverflow = body.style.overflow;
-
-    html.style.overflow = "hidden";
-    body.style.overflow = "hidden";
-
-    /* cleanup on route‑change / unmount */
-    return () => {
-      html.style.overflow = htmlOverflow;
-      body.style.overflow = bodyOverflow;
-    };
-  }, []);
   return (
-    <div className = "relative flex flex-col items-center justify-center h-screen overflow-hidden lg:justify-normal lg:pl-64"> {/*defining a flex box flex col so our text, header, and image all stack on each other vertically and also moving the container for all these things to the right rigth by 36 */}
-      
-      <div className = "flex flex-col items-start justify-center lg:pt-48"> {/*wrapping the header and text in their own flex box so I can use items start to center them left across their cross axis which in this case is the horizontal one */}
-        <Header header = "Home" icon = "Home" />
-        <p className = "text-white transform text-3xl max-w-60 sm:text-4xl sm:max-w-lg md:text-5xl md:max-w-xl lg:text-6xl lg:max-w-3xl animate-flip-down animate-duration-[2000ms] animate-ease-out animate-normal animate-fill-forwards">{introductionText}</p>
-      </div>
-      
-      <Image className = "select-none absolute bottom-[calc(env(safe-area-inset-bottom)-2rem)] sm:-bottom-20" src = "/images/mtFuji.png" alt = "Mount Fuji" width = {800} height = {800}/>
+    <>
+      <ScrollLock/>
+      <div className = "relative flex flex-col items-center justify-center h-screen overflow-hidden lg:justify-normal lg:pl-64"> {/*defining a flex box flex col so our text, header, and image all stack on each other vertically and also moving the container for all these things to the right rigth by 36 */}
+    
+        <div className = "flex flex-col items-start justify-center lg:pt-48"> {/*wrapping the header and text in their own flex box so I can use items start to center them left across their cross axis which in this case is the horizontal one */}
+          <Header header = "Home" icon = "Home" />
+          <p className = "text-white transform text-3xl max-w-60 sm:text-4xl sm:max-w-lg md:text-5xl md:max-w-xl lg:text-6xl lg:max-w-3xl animate-flip-down animate-duration-[2000ms] animate-ease-out animate-normal animate-fill-forwards z-10">{introductionText}</p>
+        </div>
+        
+        <Image className = "select-none absolute bottom-6 sm:-bottom-20 " src = "/images/mtFuji.png" alt = "Mount Fuji" width = {800} height = {800}/>
 
-    </div>
+      </div>
+    </>
   );
 }
