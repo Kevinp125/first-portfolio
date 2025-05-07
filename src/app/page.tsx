847801473e646ba3f0c1d7e61d1d9702b778d3d8
Kevin Pereda <kptwdo15@gmail.com>
2025-05-07 13:48:41 -0400

    Added breakpoints so that the landing page is fully responsive and will appear good on mobile screens.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,14 @@ const introductionText: string = "Hi, I'm Kevin Pereda — software engineer and
 
 export default function Home() {
   return (
-    <div className = "relative flex flex-col items-center h-screen overflow-hidden pl-64"> {/*defining a flex box flex col so our text, header, and image all stack on each other vertically and also moving the container for all these things to the right rigth by 36 */}
+    <div className = "relative flex flex-col items-center justify-center h-screen overflow-hidden lg:justify-normal lg:pl-64"> {/*defining a flex box flex col so our text, header, and image all stack on each other vertically and also moving the container for all these things to the right rigth by 36 */}
       
-      <div className = "flex flex-col items-start justify-center pt-48"> {/*wrapping the header and text in their own flex box so I can use items start to center them left across their cross axis which in this case is the horizontal one */}
+      <div className = "flex flex-col items-start justify-center lg:pt-48"> {/*wrapping the header and text in their own flex box so I can use items start to center them left across their cross axis which in this case is the horizontal one */}
         <Header header = "Home" icon = "Home" />
-        <p className = "text-white transform text-6xl max-w-3xl animate-flip-down animate-duration-[2000ms] animate-ease-out animate-normal animate-fill-forwards">{introductionText}</p>
+        <p className = "text-white transform text-3xl max-w-60 sm:text-4xl sm:max-w-lg md:text-5xl md:max-w-xl lg:text-6xl lg:max-w-3xl animate-flip-down animate-duration-[2000ms] animate-ease-out animate-normal animate-fill-forwards">{introductionText}</p>
       </div>
       
-      <Image className = "select-none absolute -bottom-20" src = "/images/mtFuji.png" alt = "Mount Fuji" width = {800} height = {800}/>
+      <Image className = "select-none absolute -bottom-8 sm:-bottom-20" src = "/images/mtFuji.png" alt = "Mount Fuji" width = {800} height = {800}/>
 
     </div>
   );
